b3258375e054e5790c63810d644c7df97a583d85
Nofry <nofryarman@gmail.com>
2016-04-23 21:58:14 -0700

    pembuatan class hitungan

--- src/LatihanBab5.java
+++ src/LatihanBab5.java
@@ -1,13 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author irwin deriyan
- */
-public class LatihanBab1 {
+class Hitungan{
+     public static void Penjumlahan(int bilangan1, int bilangan2){
+        int nilai = bilangan1 + bilangan2;
+        System.out.println("Hasil Penjumlahan : "+nilai);
+    }
+    public static void Pengurangan(int bilangan1, int bilangan2){
+        int nilai = bilangan1 - bilangan2;
+        System.out.println("Hasil Pengurangan : "+nilai);
+    }
+    public void Perkalian(int bilangan1, int bilangan2){
+        int nilai = bilangan1 * bilangan2;
+        System.out.println("Hasil Perkalian : "+nilai);
+    }
+    public void Pembagian(int bilangan1, int bilangan2){
+        double nilai = (double) bilangan1 / bilangan2;
+        System.out.println("Hasil Pembagian : "+nilai);
+    }
+    public void Sederhana(int bilangan1, int bilangan2){
+        int temp, a, b;
+        if(bilangan2==0){
+            return;
+        }
+        a = (bilangan1 < bilangan2) ? bilangan2:bilangan1;
+        b = (bilangan1 < bilangan2) ? bilangan1:bilangan2;
+        while(b!=0){
+            temp = a%b;
+            a=b;
+            b=temp;
+        }
+        bilangan1 /=a;
+        bilangan2 /=a;
+        System.out.println("Hasil Penyederhanaan : "+bilangan1+"/"+bilangan2);
+    }
+}
+public class LatihanBab5 {
     public static void main(String[] args) {
         System.out.println("tess");
         
