786a397d3c39f2b99150e2efca23a6a21ac54d1f
Nofry <nofryarman@gmail.com>
2016-04-23 22:15:50 -0700

    membuat method pembelian

--- src/tugas/bab5/TugasBab5.java
+++ src/tugas/bab5/TugasBab5.java
@@ -6,6 +6,19 @@ class Jaket{
     public int potongan[]= {5000,5000,15000};
     public int harga[] = new int[3];
     public int Total;
+    public void Pembelian(int a[]){
+        jumlah = a;
+        Total = 0;
+        for (int i = 0; i < 3; i++) {
+            if(a[i]>100){
+                harga[i] = (jaket[i] * a[i])-(potongan[i]*a[i]);
+            }
+            else{
+                harga[i]= (jaket[i] * a[i]);
+            }
+            Total += harga[i];
+        }
+    }
 }
 public class TugasBab5 {
 
